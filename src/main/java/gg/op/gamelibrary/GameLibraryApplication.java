cd1734b557a373203b0e1ce37e1b2b4252a34600
raeperd <raeperd117@gmail.com>
2021-07-16 19:48:42 +0900

    First commit

---
+++ src/main/java/gg/op/gamelibrary/GameLibraryApplication.java
@@ -0,0 +1,13 @@
+package gg.op.gamelibrary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GameLibraryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GameLibraryApplication.class, args);
+    }
+
+}
---
+++ src/test/java/gg/op/gamelibrary/GameLibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package gg.op.gamelibrary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GameLibraryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
