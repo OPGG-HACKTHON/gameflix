da99ca62232f833aa6b10fa1385bdfec03678249
jeongjiwoo0522 <jiwoourty@gmail.com>
2021-08-19 09:39:05 +0900

    #8 Implements BlizzardClient

--- src/main/kotlin/gg/op/gameflix/infrastructure/blizzard/BlizzardService.kt
+++ src/main/kotlin/gg/op/gameflix/infrastructure/blizzard/BlizzardService.kt
@@ -1,15 +1,14 @@
 package gg.op.gameflix.infrastructure.blizzard
 
-import gg.op.gameflix.domain.game.GameSlug
 import gg.op.gameflix.domain.game.GameStoreAuthentication
 import gg.op.gameflix.domain.game.GameStoreService
 
-class BlizzardService : GameStoreService {
-    override fun getAllGameSlugsByAuthentication(authentication: GameStoreAuthentication): Collection<GameSlug> {
-        TODO("Not yet implemented")
-    }
+class BlizzardService(private val client: BlizzardClient) : GameStoreService {
+    override fun getAllGameSlugsByAuthentication(authentication: GameStoreAuthentication)
+        = client.queryGetGames(authentication as BlizzardAuthentication)
 
-    override fun supports(authentication: GameStoreAuthentication): Boolean {
-        TODO("Not yet implemented")
-    }
+    override fun supports(authentication: GameStoreAuthentication)
+        = authentication is BlizzardAuthentication
 }
+
+data class BlizzardAuthentication(val accessToken: String) : GameStoreAuthentication
