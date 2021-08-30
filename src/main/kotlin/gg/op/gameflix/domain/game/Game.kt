5d5525205698fe12bddd72b58924bfc16fea7c9e
raeperd <raeperd117@gmail.com>
2021-08-30 20:42:35 +0900

    #30 Fix Game class with additional fields

--- src/main/kotlin/gg/op/gameflix/domain/game/Game.kt
+++ src/main/kotlin/gg/op/gameflix/domain/game/Game.kt
@@ -2,22 +2,42 @@ package gg.op.gameflix.domain.game
 
 import java.net.URI
 
+data class Game(
+    val summary: GameSummary,
+    val detail: GameDetail
+)
+
 data class GameSummary(
     val slug: GameSlug,
     val cover: URI
 )
 
-data class Game(val slug: GameSlug,
-    val detail: GameDetail,
-    val genres: Collection<Genre>,
-    val platforms: Collection<Platform>,
+data class GameDetail(
+    val releaseAt: Int,
+    val updatedAt: Int,
+    val url: String,
+    val description: String,
+    val companiesInvolved: Set<Company>,
+    val genres: Set<Genre>,
+    val platforms: Set<Platform>,
     val rating: GameRating
 )
 
-data class GameDetail(val releaseAt: Int, val updatedAt: Int, val cover: String, val url: String)
+data class GameSlug(val name: String, val slug: String) {
+    constructor(name: String) : this(name, name.toSlug())
+}
 
-enum class Genre
+private fun String.toSlug() = lowercase()
+    .replace("\n", " ")
+    .replace("[^a-z\\d\\s]".toRegex(), " ")
+    .split(" ")
+    .joinToString("-")
+    .replace("-+".toRegex(), "-")
 
-enum class Platform
+data class Company(val name: String)
+
+data class Genre(val name: String)
+
+data class Platform(val name: String)
 
 data class GameRating(val rating: Float, val count: Int)
