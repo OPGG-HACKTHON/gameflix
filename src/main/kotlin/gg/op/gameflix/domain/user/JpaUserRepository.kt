6d8dbc739a5ee583b2ca89f440e5c611e4478dbf
jeongjiwoo0522 <jiwoourty@gmail.com>
2021-08-31 23:50:50 +0900

    #9 Add JpaUserRepository

---
+++ src/main/kotlin/gg/op/gameflix/domain/user/JpaUserRepository.kt
@@ -0,0 +1,8 @@
+package gg.op.gameflix.domain.user
+
+import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.stereotype.Repository
+
+@Repository
+interface JpaUserRepository : JpaRepository<User, Long>, UserRepository {
+}
